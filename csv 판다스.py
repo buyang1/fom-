91902ea9c5f8ab5bbda5e169215c3e70b614aab0
buyang1 <115389994+buyang1@users.noreply.github.com>
2025-03-19 17:08:33 +0900

    Add files via upload

---
+++ csv 판다스.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+df1 = pd.read_csv("train.csv")
+df1
+
+
+# In[3]:
+
+
+import pandas as pd
+df2 = pd.read_csv("test.csv")
+df2
+
+
+# In[7]:
+
+
+df1.shape
+
+
+# In[9]:
+
+
+df1.info()
+
+
+# In[ ]:
+
+
+
+
